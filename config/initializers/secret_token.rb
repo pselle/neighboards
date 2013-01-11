8cde004eb061a36c6775a29a52ea035baa2de398
Pam Selle <pamela.selle@gmail.com>
2013-01-11 12:49:59 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neighboards::Application.config.secret_token = 'fd952ee81fe02f00e97267b81aac4986a67865a3f340a5cf60c14bea217eba8ca0138f75b696618a9f27ac774784ea721b46dcc56f9c63156f738cd6e1a58bfe'
